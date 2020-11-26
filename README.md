5fbc58cabc6e03ce01e4ae338a64026492aeb933
Manale <chakirmanale@gmail.com>
2020-11-26 06:24:46 -0800

    added new doctypes

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 ## Meeting
 
-<<<<<<< HEAD
-App to manage meetings
-=======
 meeting management app
->>>>>>> 9fb53e762bb1c96f5a4dfb993f8b001ff7b87770
 
 #### License
 
--- meeting/config/desktop.py
+++ meeting/config/desktop.py
@@ -6,13 +6,9 @@ def get_data():
 	return [
 		{
 			"module_name": "Meeting",
-<<<<<<< HEAD
 			"color": "blue",
-=======
-			"color": "grey",
->>>>>>> 9fb53e762bb1c96f5a4dfb993f8b001ff7b87770
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Meeting")
-		}
-	]
+		
+		}]
