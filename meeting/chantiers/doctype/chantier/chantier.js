1bf5d5fedec95cef118ba05a844cc1c0a6b4fdf8
Manale <chakirmanale@gmail.com>
2020-12-10 00:56:00 -0800

    added module 'chantiers'

---
+++ meeting/chantiers/doctype/chantier/chantier.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Manale and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Chantier', {
+	// refresh: function(frm) {
+
+	// }
+});
--- meeting/config/desktop.py
+++ meeting/config/desktop.py
@@ -6,10 +6,20 @@ def get_data():
 	return [
 		{
 			"module_name": "Meeting",
+			"category": "Modules",
 			"color": "black",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
 			"label": _("Meeting Management")
 		
+		},
+		{
+			"module_name": "Chantiers",
+			"category": "Modules",
+			"color": "black",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Chantiers")
 		}
+
 	]
