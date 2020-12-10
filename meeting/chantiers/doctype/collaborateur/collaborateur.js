ec166c453deb7f59dc889e7cbfcc1b7acffa9452
Manale <chakirmanale@gmail.com>
2020-12-10 01:50:16 -0800

    doctype Collab

---
+++ meeting/chantiers/doctype/collaborateur/collaborateur.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Manale and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Collaborateur', {
+	// refresh: function(frm) {
+
+	// }
+});
